bfa4461fa0df8aa4e1816d0d7d5ca78dc0db3aae
Rahman Sharmin <s1800091@student.businesscollege.fi>
2019-01-22 15:37:45 +0200

    Updates

---
+++ precache-manifest.deafb6af75cdb7d0cafc813729414504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "edcfa8a852f959e2feef",
+    "url": "/Weather-App/static/js/runtime~main.edcfa8a8.js"
+  },
+  {
+    "revision": "5f934a137c8ec89e85bf",
+    "url": "/Weather-App/static/js/main.5f934a13.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/Weather-App/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "5f934a137c8ec89e85bf",
+    "url": "/Weather-App/static/css/main.1efff61c.chunk.css"
+  },
+  {
+    "revision": "3fa19200c8e0447dd9f3c351b733c008",
+    "url": "/Weather-App/index.html"
+  }
+];
